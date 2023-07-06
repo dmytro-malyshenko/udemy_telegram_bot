a56c41a5f8eec6394decde518f91d4aa00cb5036
Dmytro Malyshenko <malishenko.dima@gmail.com>
2023-07-06 19:45:55 +0300

    Migration python-teleagram-bot from v13 to v20

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from telegram.ext import (Updater, CommandHandler, ConversationHandler, MessageHandler,
-                          Filters, CallbackContext)
+import asyncio
+
+from telegram.ext import (CommandHandler, ConversationHandler, MessageHandler,
+                          filters, ContextTypes, ApplicationBuilder)
 from telegram import KeyboardButton, ReplyKeyboardMarkup, Update
 from data_source import DataSource
 import os
@@ -22,19 +24,19 @@ logger = logging.getLogger()
 if MODE == "dev":
     def run():
         logger.info("Start in DEV mode")
-        updater.start_polling()
+        application.run_polling()
 elif MODE == "prod":
     def run():
         logger.info("Start in PROD mode")
-        updater.start_webhook(listen="0.0.0.0", port=int(os.environ.get("PORT", "8443")), url_path=TOKEN,
-                              webhook_url="https://{}.herokuapp.com/{}".format(os.environ.get("APP_NAME"), TOKEN))
+        application.run_webhook(listen="0.0.0.0", port=int(os.environ.get("PORT", "8443")), url_path=TOKEN,
+                                webhook_url="https://{}.herokuapp.com/{}".format(os.environ.get("APP_NAME"), TOKEN))
 else:
     logger.error("No mode specified!")
     sys.exit(1)
 
 
-def start_handler(update, context):
-    update.message.reply_text("Hello, creator!", reply_markup=add_reminder_button())
+async def start_handler(update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello, creator!", reply_markup=add_reminder_button())
 
 
 def add_reminder_button():
@@ -42,53 +44,57 @@ def add_reminder_button():
     return ReplyKeyboardMarkup(keyboard)
 
 
-def add_reminder_handler(update: Update, context: CallbackContext):
-    update.message.reply_text("Please enter a message of the reminder:")
+async def add_reminder_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Please enter a message of the reminder:")
     return ENTER_MESSAGE
 
 
-def enter_message_handler(update: Update, context: CallbackContext):
-    update.message.reply_text("Please enter a time when bot should remind:")
+async def enter_message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Please enter a time when bot should remind:")
     context.user_data["message_text"] = update.message.text
     return ENTER_TIME
 
 
-def enter_time_handler(update: Update, context: CallbackContext):
+async def enter_time_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_text = context.user_data["message_text"]
     time = datetime.datetime.strptime(update.message.text, "%d/%m/%Y %H:%M")
     message_data = dataSource.create_reminder(update.message.chat_id, message_text, time)
-    update.message.reply_text("Your reminder: " + message_data.__repr__())
+    await update.message.reply_text("Your reminder: " + message_data.__repr__())
     return ConversationHandler.END
 
 
 def start_check_reminders_task():
-    thread = threading.Thread(target=check_reminders, args=())
+    thread = threading.Thread(target=wrapped_async_mcheck_reminders, args=())
     thread.daemon = True
     thread.start()
 
 
-def check_reminders():
+async def check_reminders():
     while True:
         for reminder_data in dataSource.get_all_reminders():
             if reminder_data.should_be_fired():
                 dataSource.fire_reminder(reminder_data.reminder_id)
-                updater.bot.send_message(reminder_data.chat_id, reminder_data.message)
+                await application.bot.send_message(reminder_data.chat_id, reminder_data.message)
         time.sleep(INTERVAL)
 
 
+def wrapped_async_mcheck_reminders():
+    asyncio.run(check_reminders())
+
+
 if __name__ == '__main__':
-    updater = Updater(TOKEN, use_context=True)
-    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
+    application = ApplicationBuilder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start_handler))
     conv_handler = ConversationHandler(
-        entry_points=[MessageHandler(Filters.regex(ADD_REMINDER_TEXT), add_reminder_handler)],
+        entry_points=[MessageHandler(filters.Regex(ADD_REMINDER_TEXT), add_reminder_handler)],
         states={
-            ENTER_MESSAGE: [MessageHandler(Filters.all, enter_message_handler)],
-            ENTER_TIME: [MessageHandler(Filters.all, enter_time_handler)]
+            ENTER_MESSAGE: [MessageHandler(filters.ALL, enter_message_handler)],
+            ENTER_TIME: [MessageHandler(filters.ALL, enter_time_handler)]
         },
         fallbacks=[]
     )
-    updater.dispatcher.add_handler(conv_handler)
+    application.add_handler(conv_handler)
     dataSource.create_tables()
-    run()
     start_check_reminders_task()
+    run()
 
